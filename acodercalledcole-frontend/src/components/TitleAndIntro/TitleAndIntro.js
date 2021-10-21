04959e93311bda4fa0caf9378ad0d5c2b29c2118
brett cole <acodercalledcole@gmail.com>
2021-10-21 15:51:56 -0400

    working with title and subtitle

--- acodercalledcole-frontend/src/components/TitleAndIntro/TitleAndIntro.js
+++ acodercalledcole-frontend/src/components/TitleAndIntro/TitleAndIntro.js
@@ -3,20 +3,20 @@ import './TitleAndIntro.css';
 import {Col, Container, Row} from 'react-bootstrap';
 
 const TitleAndIntro = () => {
-  return <Container fluid>
-    <Row>
-      <Col xs={1}><p>placeholder</p></Col>
+  return <Container fluid className='vh-100' style={{backgroundImage: "linear-gradient(to right, #0f0c29, #302b63, #24243e"}}>
+    <Row className='mb-5'>
+      <Col xs={1} className='text-white'><p>placeholder</p></Col>
       <Col xs={8}></Col>
-      <Col xs={1}><p>placeholder</p></Col>
+      <Col xs={1} className='text-white'><p>placeholder</p></Col>
     </Row>
+    <Row className='mt-5 mb-5' />
     <Row className='mt-5'>
-      <Col xs={2} sm={4} lg={5} xl={6} xxl={7} />
-      <Col className='bg-success bg-gradient' xs={9} sm={7} lg={6} xl={5} xxl={5}><p className='fullstack-title text-end text-white'>Fullstack</p></Col>
-      <Col />
+      <Col xs={2} sm={4} lg={6} xl={7} xxl={7} />
+      <Col className='bg-success bg-gradient fullstack-title-parent' xs={9} sm={7} lg={6} xl={5} xxl={5}><p className='fullstack-title text-end text-white'>Fullstack</p></Col>
     </Row>
     <Row>
       <Col xs={5} sm={6} md={7} lg={8}></Col>
-      <Col xs={'auto'}><p className='fw-bold fullstack-subTitle text-start'>developer</p></Col>
+      <Col xs={'auto'}><p className='fw-bold fullstack-subTitle text-start text-white'>developer</p></Col>
       <Col xs={2}></Col>
     </Row>
     <Row className='mt-5'>
